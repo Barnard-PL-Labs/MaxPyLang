d115152661d4fcf78f7b639471fc5d89e5d26845
sleepyheadmp3 <jenniferfu03@gmail.com>
2025-11-12 11:26:40 -0500

    updated name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='MaxPy',
+    name='MaxPy-lang',
     version='0.1.0',
     author='Ranger Liu',
     author_email='ranger.liu@columbia.edu',
